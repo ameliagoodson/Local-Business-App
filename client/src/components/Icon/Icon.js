a07a8b5839703226f9ac98fd6afdbafe309ccf96
Amelia Goodson <ameliajanegoodson@gmail.com>
2020-07-31 21:47:00 +1000

    remove unnecessary files

--- client/src/components/Icon/Icon.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import icon from "./components/Icon"; // Tell webpack this JS file uses this image
-
-function Icon() {
-  return <img src={icon} alt="icon" />;
-}
-export default Icon;
